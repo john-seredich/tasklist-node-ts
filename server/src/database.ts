a27b384dcf06194442633a3961f8b979a6bcd54c
john-seredich <seredichjohnalt@gmail.com>
2023-11-08 16:00:33 -0500

    feat: create connection to sql database, basic func to data

---
+++ server/src/database.ts
@@ -0,0 +1,20 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
+
+const pool = mysql
+  .createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  })
+  .promise();
+
+async function getNotes() {
+  const [rows] = await pool.query("SELECT * FROM tasks");
+  return rows;
+}
+
+const notes = await getNotes();
+console.log(notes);
