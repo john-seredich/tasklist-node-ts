5335f26887a835c5353ac694f4a74afd7dc47848
john-seredich <seredichjohnalt@gmail.com>
2023-11-12 13:05:03 -0500

    feat: create GET endpoints to return task information

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,31 @@
-import express, { Express, Request, Response } from "express";
-const app: Express = express();
-const port = 3000;
+import express from "express";
+import { getNote, getNotes, createNote } from "./database.js";
+const port = 8080;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
+const app = express();
+app.use(express.json());
+
+app.get("/tasks", async (req, res) => {
+  const notes = await getNotes();
+  res.send(notes);
 });
 
-app.get("/hi", (req: Request, res: Response) => {
-  res.send("Hio");
+app.get("/tasks/:id", async (req, res) => {
+  // Add handling for empty case
+  const id = req.params.id;
+  const task = await getNote(Number(id));
+  res.send(task);
+});
+
+app.post("/tasks", async (req, res) => {
+  const { title, content } = req.body;
+  const task = await createNote(title, content);
+  res.status(201).send(task);
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 app.listen(port, () => {
